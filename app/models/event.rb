d950b022ac94ccfc57a658595636f0cacad1be3a
Rafael Matuoka <rmatuoka@agenciaeconomica.com.br>
2010-06-08 12:51:46 -0300

    Ajuste eventos

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,7 +6,7 @@ class Event < ActiveRecord::Base
     validates_presence_of :date_start
     validates_presence_of :date_end
     
-    has_attached_file :image, :styles => { :thumb => "125x125>", :home => "202x36>" },
+    has_attached_file :image, :styles => { :thumb => "125x125>", :home_evt => "202x36>" },
         :url => "/uploads/:attachment/:id/:basename_:style.:extension",
         :path => ":rails_root/public/uploads/:attachment/:id/:basename_:style.:extension"
 end
